107a22dd80c7a627c4362672ab7ead3b0c002cf6
Saeid <43203342+Saeidhoseinipour@users.noreply.github.com>
2023-02-01 20:20:57 +0330

    Update __init__.py

--- Models/__init__.py
+++ Models/__init__.py
@@ -11,20 +11,12 @@ algorithms with Nonnegative Matrix Tri Factorization.
 """
 
 from .NMTFcoclust_OPNMTF_alpha import OPNMTF
-from .NMTFcoclust_ONMTF_alpha import ONMTF
-from .NMTFcoclust_NMTF_alpha import NMTF
-from .NMTFcoclust_NBVD import NBVD
-from .NMTFcoclust_DNMF import DNMF
-#from .NMTFcoclus_ODNMF import ODNMF
-from .NMTFcoclust_ONM3F import ONM3F
-from .NMTFcoclust_ONM3F_2 import ONM3F_2
+from .NMTFcoclust_ONMTF_alpha import CoclustELBMcem
+from .NMTFcoclust_NMTF_alpha import CoclustSELBMcem
+
 
 
 __all__ = ['OPNMTF',
-		'NBVD',
-		'DNMF',
-		'ONM3F',
-		'ONM3F_2',
-		'ONMTF',
-		'NMTF']
+		'CoclustELBMcem',
+		'CoclustSELBMcem']
 
